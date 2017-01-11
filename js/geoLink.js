200152ad400b90cebb21563748e8c82d0ca7d523
Audrey Mickle <amickle@whoi.edu>
2017-01-11 10:43:28 -0500

    Add comments to JS

--- js/geoLink.js
+++ js/geoLink.js
@@ -1,20 +1,15 @@
+/* EDIT HERE: Identify string used to trigger the creation of this element */
+
 if (document.querySelector('meta[content^="Knorr (Ship : 1970-)"]')) {
 var element = document.querySelector('meta[content^="Knorr (Ship : 1970-)"]').attributes;
 }
-if (document.querySelector('meta[content^="Oceanus (Ship : 1975-)"]')) {
-var element = document.querySelector('meta[content^="Oceanus (Ship : 1975-)"]').attributes;
-}
-if (document.querySelector('meta[content^="Atlantis (Ship : 1996-)"]')) {
-var element = document.querySelector('meta[content^="Atlantis (Ship : 1996-)"]').attributes;
-}
-
 
 
 if (element) {
 
 
-
-/*if there is a matching meta element, insert a geoLink div in right column.*/
+/*if there is a matching meta element, insert a geoLink div in right column. 
+EDIT HERE: Identify the class tag used to place your object, or write new logic.*/
 
   var topElement = document.getElementsByClassName("col-sm-8");
   var newElement = document.createElement("div");
@@ -23,6 +18,7 @@ if (element) {
   topElement[0].insertBefore(newElement,topElement[0].firstChild);
   
 /*get the cruise from the element value and create gldeployment element, make contents of geoLink element*/
+  
   var evalue = element.content.value;
   var cruise = evalue.split('Cruise ')[1];
   glConstruct = "<gldeployment-element params='{\"host\": \"GeoLink\", \"hosturl\":\"http://geolink.org\", \"label\": \"" + cruise + "\"}'/>"
